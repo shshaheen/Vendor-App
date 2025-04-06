9e2cec813a3aded790e40afb3ec8d70b4b6b137a
shshaheen <kolimishaheen9885@gmail.com>
2025-04-06 19:06:43 +0530

    Bottom Navigation Bar

--- lib/views/screens/main_vendor_screen.dart
+++ lib/views/screens/main_vendor_screen.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:lucide_icons/lucide_icons.dart';
 
 class MainVendorScreen extends StatefulWidget {
   const MainVendorScreen({super.key});
@@ -8,10 +10,44 @@ class MainVendorScreen extends StatefulWidget {
 }
 
 class _MainVendorScreenState extends State<MainVendorScreen> {
+  int _pageIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("Main Vendor Screen"),),
+      body: Center(child: Text("Main Vendor Screen")),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _pageIndex,
+        onTap: (value) {
+          setState(() {
+            _pageIndex = value;
+          });
+        },
+        unselectedItemColor: Colors.grey,
+        selectedItemColor: Colors.purple,
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.money_dollar),
+            label: 'Earnings',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.upload_circle),
+            label: 'Upload',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.edit),
+            label: 'Edit',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.shopping_cart),
+            label: 'Orders',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.logout),
+            label: 'Logout',
+          ),
+        ],
+      ),
     );
   }
 }
